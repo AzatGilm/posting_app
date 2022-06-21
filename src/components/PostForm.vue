2eab45650575d969ac52eb2dbf1493bc2690ad88
Azat <ya.azat77@yandex.ru>
2022-06-21 04:30:42 +0300

    eventbus,UI, etc

---
+++ src/components/PostForm.vue
@@ -0,0 +1,63 @@
+<template>
+  <form class="form" @submit.prevent>
+    <h4> Создание поста </h4>
+    <input
+      class="input"
+      type="text"
+      placeholder="Название"
+      v-model="post.title"
+    >
+    <input
+      class="input"
+      type="text"
+      placeholder="Описание"
+      v-model="post.body"
+    >
+    <my-button
+      @click="createPost"
+      style="align-self: flex-end; margin-top: 15px"
+    >
+        Создать пост
+    </my-button>
+  </form>
+</template>
+
+<script>
+
+export default {
+  components: {
+
+  },
+  data () {
+    return {
+      post: {
+        title: '',
+        body: ''
+      }
+    }
+  },
+  methods: {
+    createPost () {
+      this.post.id = Date.now()
+      this.$emit('create', this.post)
+      this.post = {
+        title: '',
+        body: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.input {
+  width: 100%;
+  padding: 10px 15px;
+  border: 1px solid teal;
+  margin-top: 15px;
+}
+.form {
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h4> Список пользователей </h4>
+    <post-item
+      v-for="(post,i) in posts"
+      :key = "i"
+      :post = "post"
+    />
+  </div>
+</template>
+
+<script>
+import PostItem from './PostItem'
+
+export default {
+  components: {
+    PostItem
+  },
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
