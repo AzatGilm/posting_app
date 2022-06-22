061643dca306274f48c828edbd71f75159a9898f
Azat <ya.azat77@yandex.ru>
2022-06-22 08:20:37 +0300

    pagination

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
   <div v-if="posts.length>0">
     <h4> Список пользователей </h4>
-    <post-item
+    <transition-group name="post-list">
+      <post-item
       v-for="post in posts"
       :key = "post.id"
       :post = "post"
       @remove="$emit('remove', post)"
     />
+    </transition-group>
   </div>
   <h2 v-else style="color:red" > Список пользователей пуст </h2>
 </template>
@@ -31,5 +33,20 @@ export default {
 </script>
 
 <style scoped>
-
+  .post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+  }
+  .post-list-enter-active,
+  .post-list-leave-active {
+    transition: all 0.5s ease;
+  }
+  .post-list-enter-from,
+  .post-list-leave-to {
+    opacity: 0;
+    transform: translateX(130px);
+  }
+  .post-list-move {
+    transition: transform 0.5s ease;
+  }
 </style>
